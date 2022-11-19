56ecce847ea2c13364ad4bdb4ba13b25b30ff9d8
Stuart <stuart.francis@hotmail.co.uk>
2022-11-19 22:48:01 +0000

    first commit

---
+++ src/components/Alphabet.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Letter } from "./Letter";
+
+export const Alphabet = () => {
+    return (
+        <div>
+            <p>I will be the alphabet</p>
+            <Letter />
+        </div>
+    )
+};
---
+++ src/components/Letter.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Letter = () => {
+
+    return (
+        <p>I am a letter</p>
+    )
+}
